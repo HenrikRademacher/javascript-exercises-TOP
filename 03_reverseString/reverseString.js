5550a18c4b6461a1ff07e4d0fb25887b9f8c7288
HenrikRademacher <henrik.rademacher1@gmail.com>
2025-04-15 17:50:25 +0200

    Solve JS / 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(textInput) {
+    const inputArray = textInput.split("");
+    console.log(inputArray)
+    resultString = "";
+    for (let i = inputArray.length-1; i >=0; i--) {
+        resultString = resultString + inputArray[i];
+    }
+    return resultString
 };
-
+reverseString("Hello")
 // Do not edit below this line
 module.exports = reverseString;
