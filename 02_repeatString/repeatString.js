d112e34202de746bf1177098481fdc7454d7c190
HenrikRademacher <henrik.rademacher1@gmail.com>
2025-04-15 15:06:02 +0200

    Solve JS / 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(input, times) {
+    let stringResult = "";
+    if (times < 0) {
+        return "ERROR";
+    }
+    for (let i = 1; i <= times; i++) {
+        stringResult = stringResult + input;
+    }
+    return stringResult;
 };
 
 // Do not edit below this line
