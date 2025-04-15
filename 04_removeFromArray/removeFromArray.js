74ee1d5aad8120a4f22ba2e540724533c6d88d8e
HenrikRademacher <henrik.rademacher1@gmail.com>
2025-04-15 18:27:40 +0200

    Solve JS / 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...allParas) {
+    let editedArray = inputArray
+    for (removal of allParas) {
+        for (let i = 0; i <= editedArray.length-1; i++) {
+            if (editedArray[i] === removal) {
+                editedArray.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return editedArray;
 };
+
+removeFromArray([1,2,2,4,5],2,5)
 
 // Do not edit below this line
 module.exports = removeFromArray;
