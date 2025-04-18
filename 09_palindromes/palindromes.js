26671ce175ee6ef4931ad9bfc13486b6ba63d212
HenrikRademacher <henrik.rademacher1@gmail.com>
2025-04-18 14:53:32 +0200

    Solve JS / 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    inputString = inputString.toLowerCase();
+    inputString = inputString.replaceAll(" ", "").replaceAll("!", "").replaceAll(".", "").replaceAll("-", "").replaceAll(",", "");
+    let inputArray = inputString.split("");
+    let reverseArray = [];
+    for (let i = inputArray.length-1; i >= 0; i--) {
+        reverseArray.push(inputArray[i]);
+    }
+    let reverseString = reverseArray.join("");
+    return inputString === reverseString;
 };
 
 // Do not edit below this line
